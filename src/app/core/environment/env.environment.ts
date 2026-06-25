959960cedf1fc6b08822e1396c03ef6a27fe6b00
Amani Franck <amanifranck2005@gmail.com>
2026-06-25 22:14:42 +0200

    update

--- src/app/core/environment/env.environment.ts
+++ src/app/core/environment/env.environment.ts
@@ -4,4 +4,5 @@ export const environment = {
   SUPABASE_ANON_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltY3h2Z2Z0bWt2Z3JjZ2pjeGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1OTMzMDksImV4cCI6MjA5NzE2OTMwOX0.6Ccy6OLNxsGmmgL4HlQOSUHzErVa1u2Guf73gOKn51k',
   LOCAL_BACKEND_URL: 'http://localhost:5001',
+  ALIVE_BACKEND_URL: 'https://inventory-management-system-backend-t283.onrender.com'
 };
